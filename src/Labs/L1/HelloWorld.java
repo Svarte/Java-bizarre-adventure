ee9948842adf8ea5af48c6f2d74caf4259a8a2f1
IDEA <killme@ya.ru>
2017-01-14 16:52:19 +0500

    add line

--- src/Labs/L1/HelloWorld.java
+++ src/Labs/L1/HelloWorld.java
@@ -3,5 +3,6 @@ package Labs.L1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Just World");
     }
 }
